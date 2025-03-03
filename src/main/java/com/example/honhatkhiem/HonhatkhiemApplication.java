e95a50fe00f0769615fdb43b91b46dceebebd4a5
unknown <honhutkhiem123@gmail.com>
2025-03-03 08:47:43 +0700

    first commit

---
+++ src/main/java/com/example/honhatkhiem/HonhatkhiemApplication.java
@@ -0,0 +1,13 @@
+package com.example.honhatkhiem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonhatkhiemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonhatkhiemApplication.class, args);
+	}
+
+}
